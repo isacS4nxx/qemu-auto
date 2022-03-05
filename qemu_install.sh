734d6be6c56bd00bda69c9b31b15396c9fa9f797
isacS4nxx <89214830+isacS4nxx@users.noreply.github.com>
2022-03-05 19:08:37 -0300

    Delete qemu_install.sh

--- qemu_install.sh
+++
@@ -1,8 +0,0 @@
-if [[ $(lscpu | grep Virt) ]];
-then
-    echo "Your virtualization is active in the bios, continuing..."
-else
-    echo "Your virtualization is not activated, please activate it in your manufacturer's bios! canceling. $(exit)"
-fi
-    echo "Updating the system and installing qemu wait. $(apt-get update -y)";
-    echo "Installing the QEMU $(apt-cache show qemu | grep Version && apt install qemu qemu-kvm -y > /dev/null)"
